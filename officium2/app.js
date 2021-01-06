448bf94b7430398add58e2bdd7f9c1bf3a20b4ed
Elisa Lozano <71267623+elozano42@users.noreply.github.com>
2021-01-06 18:25:11 +0100

    Add files via upload

---
+++ officium2/app.js
@@ -0,0 +1,16 @@
+//*Ancrage 
+
+$(function () {
+    $('nav a[href^="#"]').click(function () {
+        var the_id = $(this).attr("href");
+        if (the_id === "#") {
+            return;
+        }
+        var posCible = $(the_id).offset().top - 50;
+        $('html, body').animate({
+            scrollTop: posCible
+        }, 'slow');
+        return false;
+    });
+})
+
